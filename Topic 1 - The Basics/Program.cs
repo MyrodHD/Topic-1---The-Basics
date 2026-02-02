291062529928803f1483e8e4880ba797511a605e
339742660 <339742660@2260I-R31-004>
2026-02-02 13:18:11 -0500

    Add project files.

---
+++ Topic 1 - The Basics/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Topic_1___The_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string aFriend = "Bill";
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+
+            Console.WriteLine(aFriend);
+
+            Console.WriteLine("Hello " + aFriend);
+
+            Console.WriteLine($"Hello {aFriend}");
+
+            Console.WriteLine($"My Friends are {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters");
+
+            string greeting = "    Hello World    ";
+            Console.WriteLine($"[{greeting}]");
+        }
+    }
+}
